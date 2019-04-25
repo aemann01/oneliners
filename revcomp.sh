353d9bcf968bbcb0986b986f0ec96fc486c2817f
aemann01 <aemann01@ou.edu>
2019-04-25 16:08:52 -0700

    remove wonky reverse complement script

--- revcomp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#must be used with wordwrap removed file
-#use: revcomp.sh input.fa > revcomp.fa
-
-cat $1 | while read L; do echo $L; read L; echo "$L" | rev | tr "ATGC" "TACG" ; done
-
